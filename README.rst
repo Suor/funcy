20d2698d6b0fe0fd7c0b51e36e50dada185710c9
Alexander Schepanovski <suor.web@gmail.com>
2012-10-13 06:44:37 +0800

    Initial layout

---
+++ README.rst
@@ -0,0 +1,32 @@
+Funcy
+=====
+
+A collection of fancy functional tools. Manipulate your functions and data with ease.
+
+Inspired by my own abstractions and clojure standard library.
+
+
+Installation
+-------------
+
+::
+
+    pip install funcy # someday
+
+
+Overview
+--------
+
+. . . to be filled . . .
+
+
+How you can help
+----------------
+
+- bring your ideas and/or code that can make functional python more fun
+
+
+TODO
+----
+
+- refactor or get rid of decorators
---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name='funcy',
+    version='0.0.1',
+    author='Alexander Schepanovski',
+    author_email='suor.web@gmail.com',
+
+    description='A fancy functional tools.',
+    long_description=open('README.rst').read(),
+    url='http://github.com/Suor/funcy',
+    license='BSD',
+
+    packages=['funcy'],
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
