6a4e3cb29c2304a9c233a092b4ac1b74639a2032
Alexander Schepanovski <suor.web@gmail.com>
2012-10-16 08:13:46 +0800

    More seq utilities

--- funcy/seqs.py
+++ funcy/seqs.py
@@ -1,4 +1,5 @@
-from itertools import repeat, islice
+from itertools import repeat, islice, ifilter, imap, chain
+from whatever import _, that
 
 # NOTE: Should I import here anything from itertools or elsewhere,
 #       if it exactly implements some function I want?
@@ -20,6 +21,50 @@ def take(n, coll):
     return list(islice(coll, n))
 
 # TODO: tree-seq equivalent
+
+def distinct(seq):
+    "Order preserving distinct"
+    seen = set()
+    return [x for x in seq if x not in seen and not seen.add(x)]
+
+def remove(pred, coll):
+    return filter(complement(pred), coll)
+
+def iremove(pred, coll):
+    return ifilter(complement(pred), coll)
+
+def concat(*colls):
+    return list(chain(*colls))
+iconcat = chain                # clojure's concat
+iiconcat = chain.from_iterable # clojure's lazy-cat
+
+def mapcat(f, *colls):
+    return concat(*map(f, *colls))
+
+def imapcat(f, *colls):
+    return chain.from_iterable(map(f, *colls))
+
+# interleave = izip
+
+def interpose(sep, coll):
+    pass
+
+
+def drop(n, seq):
+    return islice(seq, n, None)
+
+
+def keep(f, seq):
+    return remove(_ is None, map(f, seq))
+
+def ikeep(f, seq):
+    return iremove(_ is None, imap(f, seq))
+
+def keep_indexed(f, seq):
+    raise NotImplementedError
+
+def ikeep_indexed(f, seq):
+    raise NotImplementedError
 
 
 from itertools import count
