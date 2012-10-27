a9779cf62b1ceda49ae5d26c92cc5a8d2900bbf0
Alexander Schepanovski <suor.web@gmail.com>
2012-10-27 09:20:59 +0800

    Some bits from numbers sheet

---
+++ funcy/numbers.py
@@ -0,0 +1,25 @@
+### Arithmetic
+#
+# + - * / translate to same ops in python
+# NOTE: `from __future__ import division` should be used to make / behave the same
+#
+# quot = //
+# mod  = %
+# TODO: rem (like `mod` but slightly different)
+
+def inc(x):
+    return x + 1
+
+def dec(x):
+    return x - 1
+
+# max, min - builtins
+
+
+### Compare
+#
+# = == not= < > <= >= roughly the same in python
+# compare = cmp
+
+
+### Bitwise, Cast, Test, Random, BigDecimal and Unchecked - skipped for now
