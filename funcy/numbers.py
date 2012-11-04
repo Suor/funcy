27598a024c9a12343976b004f0e7fa852b4e35a6
Alexander Schepanovski <suor.web@gmail.com>
2012-11-05 04:19:32 +0800

    Finished first numbers release

--- funcy/numbers.py
+++ funcy/numbers.py
@@ -1,25 +1,12 @@
-### Arithmetic
-#
-# + - * / translate to same ops in python
-# NOTE: `from __future__ import division` should be used to make / behave the same
-#
-# quot = //
-# mod  = %
-# TODO: rem (like `mod` but slightly different)
-
 def inc(x):
     return x + 1
 
 def dec(x):
     return x - 1
 
-# max, min - builtins
 
+def even(x):
+    return x % 2 == 0
 
-### Compare
-#
-# = == not= < > <= >= roughly the same in python
-# compare = cmp
-
-
-### Bitwise, Cast, Test, Random, BigDecimal and Unchecked - skipped for now
+def odd(x):
+    return x % 2 == 1
