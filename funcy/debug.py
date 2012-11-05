36edbfd7c9960c1868e435a10091a7430af620e3
Alexander Schepanovski <suor.web@gmail.com>
2012-11-05 19:45:46 +0800

    Rewrote debug decorators with new decorator decorator

--- funcy/debug.py
+++ funcy/debug.py
@@ -1,31 +1,22 @@
 # -*- coding: utf-8 -*-
 from __future__ import print_function
 
-from datetime import datetime, timedelta
-from functools import wraps
+from .decorators import decorator
 
 
-def log(print_func=print):
-    def decorator(func):
-        @wraps(func)
-        def wrapper(*args, **kwargs):
-            arg_words = list(args) + ['%s=%s' % t for t in kwargs.items()]
-            print_func('Call %s(%s)' % (func.__name__, ', '.join(map(str, arg_words))))
-            result = func(*args, **kwargs)
-            print_func('-> %s' % repr(result))
-            return result
-        return wrapper
-    return decorator
+@decorator
+def log(call, print_func=print):
+    arg_words = list(call.args) + ['%s=%s' % t for t in call.kwargs.items()]
+    print_func('Call %s(%s)' % (call.func.__name__, ', '.join(map(str, arg_words))))
+    result = call()
+    print_func('-> %s' % repr(result))
+    return result
 
 
-def log_errors(print_func=print):
-    def decorator(func):
-        @wraps(func)
-        def wrapper(*args, **kwargs):
-            try:
-                return func(*args, **kwargs)
-            except Exception as e:
-                print_func('%s: %s in %s' % (e.__class__.__name__, e, func.__name__))
-                raise
-        return wrapper
-    return decorator
+@decorator
+def log_errors(call, print_func=print):
+    try:
+        return call()
+    except Exception as e:
+        print_func('%s: %s in %s' % (e.__class__.__name__, e, call.func.__name__))
+        raise
