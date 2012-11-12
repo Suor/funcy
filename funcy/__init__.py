6102d00f2d88ec64c44d0b8dbd25305d33c675a6
Suor <suor.web@gmail.com>
2012-11-12 17:08:57 +0800

    Reexport stuff from core funcy

---
+++ funcy/__init__.py
@@ -0,0 +1,10 @@
+from .calc import memoize # partially experimental
+from .colls import *
+from .decorators import *
+from .funcolls import *
+from .funcs import *
+from .numbers import *
+from .seqs import *
+from .strings import *
+from .flow import ignore, silent, retry # partially experimental
+# from debug import * # fully experimental
