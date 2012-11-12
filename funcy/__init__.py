a842c83e0aa1a8992a711812f40d7304b7e726b6
Suor <suor.web@gmail.com>
2012-11-12 17:20:01 +0800

    Moved primitives from numbers to __init__.py

--- funcy/__init__.py
+++ funcy/__init__.py
@@ -8,3 +8,16 @@ from .seqs import *
 from .strings import *
 from .flow import ignore, silent, retry # partially experimental
 # from debug import * # fully experimental
+
+
+def inc(x):
+    return x + 1
+
+def dec(x):
+    return x - 1
+
+def even(x):
+    return x % 2 == 0
+
+def odd(x):
+    return x % 2 == 1
