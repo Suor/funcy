013fc8d09d5077fa8114e9e735d5a767e6a599a4
Alexander Schepanovski <suor.web@gmail.com>
2012-12-18 21:38:49 +0800

    simple fetch() for django added to drafts

--- drafts/intro.py
+++ drafts/intro.py
@@ -1,1 +1,8 @@
 dict_of = lambda o: {k:getattr(o,k) for k in dir(o) if 'globals' not in k and not callable(getattr(o,k))}
+
+from django.db import connections
+
+def fetch(query, params=[], db='default'):
+    cursor = connections[db].cursor()
+    cursor.execute(query, params)
+    return cursor.fetchall()
