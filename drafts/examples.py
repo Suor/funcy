0254050630b0c03ae20914c07837d5b313ea7e62
Suor <suor.web@gmail.com>
2013-05-13 11:30:22 +0800

    Updated examples

--- drafts/examples.py
+++ drafts/examples.py
@@ -1,34 +1,11 @@
-return silent(join)(keep(f.data for f in fields))
-select_values(bool, {...}
-
-# f.error = some(chain(
-#     [f.required if f.required and not raw[f.name] else None],
-#     (r['text'] or 'internal' for r in f.rules if r['type'] == 'raw' and not r['test'](raw)),
-#     [f.required if f.required and not getattr(self, f.name) else None],
-#     (r['text'] or 'internal' for r in f.rules if r['type'] == 'rule' and not r['test'](self)),
-# ))
+has_comment = re_tester(r'#')
 
 value.value = walk_values(prepare, value.value)
 
 json_field = partial(field, json=True)
 
-key_matches = lambda key: key == '*' or category in key.split(',')
-selected = select_keys(key_matches, _categorized.value)
-# or re_tester()
-
-return map(second, sorted(selected.items()))
-
-places = re_iter(r'\b(\w+)(?:\.(\w+))?(?:\.(\d+))?\b', show) if show else []
-places = ((place, {'section': section, 'pos': int(pos) if pos else 1000000})
-          for place, section, pos in places)
-return defaultdict(lambda: defaultdict(str), places)
-
 re = lambda pattern: lambda value: re_test(pattern, value)
-
-ops = node.ops
-operands = [node.left] + node.comparators
-pairs = partition(2, 1, operands)
-node.js = ' && '.join('(%s %s %s)' % (l.js, op.js, r.js)  for op, (l, r) in zip(ops, pairs))
+re = curry(re_test)
 
 def translate_dict(d):
     lines = ('%s: %s' % (k, v) for k, v in walk_values(translate, d).items())
