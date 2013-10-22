3fbb4f491de76b652f211c6e434a1dc0699839f8
Alexander Schepanovski <suor.web@gmail.com>
2013-10-22 17:57:23 +0800

    Better document @decorator implementation

--- funcy/decorators.py
+++ funcy/decorators.py
@@ -5,21 +5,18 @@ from functools import wraps
 __all__ = ['decorator']
 
 
-def make_call(func, args, kwargs):
-    call = lambda *a, **kw: func(*(args + a), **dict(kwargs, **kw))
+def decorator(deco):
+    # Any arguments after first become decorator arguments
+    args = argcounts(deco) != (1, False, False)
 
-    # Support args and func introspection
-    call._func = func
-    call._args = args
-    call._kwargs = kwargs
+    if args:
+        # A decorator with arguments is essentialy a decorator fab
+        def decorator_fab(*dargs, **dkwargs):
+            return make_decorator(deco, dargs, dkwargs)
+        return wraps(deco)(decorator_fab)
+    else:
+        return wraps(deco)(make_decorator(deco))
 
-    # Save actual arg values on call "object"
-    if inspect.isfunction(func):
-        call_args = inspect.getcallargs(func, *args, **kwargs)
-        for arg_name, arg_value in call_args.items():
-            setattr(call, arg_name, arg_value)
-
-    return call
 
 def make_decorator(deco, dargs=(), dkwargs={}):
     def _decorator(func):
@@ -29,17 +26,29 @@ def make_decorator(deco, dargs=(), dkwargs={}):
         return wraps(func)(wrapper)
     return _decorator
 
+
+def make_call(func, args, kwargs):
+    """
+    Constructs a call object to pass as first argument to decorator.
+    Call object is just a proxy for decorated function with call arguments saved in its attributes.
+    """
+    call = lambda *a, **kw: func(*(args + a), **dict(kwargs, **kw))
+
+    # Support args and func introspection
+    call._func = func
+    call._args = args
+    call._kwargs = kwargs
+
+    # Save actual arg values on call "object" with their names as attributes for easier access
+    if inspect.isfunction(func):
+        call_args = inspect.getcallargs(func, *args, **kwargs)
+        for arg_name, arg_value in call_args.items():
+            setattr(call, arg_name, arg_value)
+
+    return call
+
+
 def argcounts(func):
     spec = inspect.getargspec(func)
     return (len(spec.args), bool(spec.varargs), bool(spec.keywords))
 
-def decorator(deco):
-    # Any arguments after first become decorator arguments
-    args = argcounts(deco) != (1, False, False)
-
-    if args:
-        def decorator_fab(*dargs, **dkwargs):
-            return make_decorator(deco, dargs, dkwargs)
-        return wraps(deco)(decorator_fab)
-    else:
-        return wraps(deco)(make_decorator(deco))
