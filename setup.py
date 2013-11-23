905993d4d80329b1af272ed20174ceef71062a41
Alexander Schepanovski <suor.web@gmail.com>
2013-11-23 11:52:03 +0800

    Added Python 2 and 2.7 classifiers

--- setup.py
+++ setup.py
@@ -18,6 +18,8 @@ setup(
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
 
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Intended Audience :: Developers',
