ac20a7a073cc3044fcb8d4fe30d0f724eecd7cfc
Alexander Schepanovski <suor.web@gmail.com>
2014-01-05 11:35:59 +0800

    Export list-returning zip() from py2 for python 3

--- funcy/py2.py
+++ funcy/py2.py
@@ -12,3 +12,13 @@ from .objects import *
 from .namespaces import namespace
 from .debug import *
 from .primitives import *
+
+
+# Python 2 style zip() for Python 3
+import sys
+
+if sys.version_info[0] == 3:
+    _zip = zip
+    def zip(*seqs):
+        return list(_zip(*seqs))
+
