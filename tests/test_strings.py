c0e0cba22276cdf2b3df3df4532e6ec6283cd047
Alexander Schepanovski <suor.web@gmail.com>
2014-04-26 18:49:40 +0800

    Completed tests for string utils

--- tests/test_strings.py
+++ tests/test_strings.py
@@ -3,6 +3,7 @@ from funcy.strings import *
 
 def test_re_find():
     assert re_find(r'\d+', 'x34y12') == '34'
+    assert re_find(r'y(\d+)', 'x34y12') == '12'
     assert re_find(r'([a-z]+)(\d+)', 'x34y12') == ('x', '34')
     assert re_find(r'(?P<l>[a-z]+)(?P<d>\d+)', 'x34y12') == {'l': 'x', 'd': '34'}
 
@@ -17,3 +18,12 @@ def test_str_join():
     assert str_join([1, 2, 3]) == '123'
     assert str_join('_', [1, 2, 3]) == '1_2_3'
     assert isinstance(str_join(u'_', [1, 2, 3]), type(u''))
+
+
+def test_cut_prefix():
+    assert cut_prefix('name:alex', 'name:') == 'alex'
+    assert cut_prefix('alex', 'name:') == 'alex'
+
+def test_cut_suffix():
+    assert cut_suffix('name.py', '.py') == 'name'
+    assert cut_suffix('name', '.py') == 'name'
