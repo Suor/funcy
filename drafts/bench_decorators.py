a94b6e159d60146dc0398003f3a19cc9803d3bc0
Alexander Schepanovski <suor.web@gmail.com>
2014-08-16 19:46:13 +0800

    Explore a new way to make decorators

--- drafts/bench_decorators.py
+++ drafts/bench_decorators.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-import gc, time, sys
+import gc, time, sys, inspect
 from functools import wraps
 
 sys.path.insert(0, '/home/suor/projects/funcy')
@@ -16,11 +16,13 @@ def empty_old_school(func):
         return func(*args, **kwargs)
     return wrapper
 
-# import wrapt
+import wrapt
 
-# @wrapt.decorator
-# def empty_wrapt(wrapped, instance, args, kwargs):
-#     return wrapped(*args, **kwargs)
+@wrapt.decorator
+def empty_wrapt(wrapped, instance, args, kwargs):
+    return wrapped(*args, **kwargs)
+
+# getx decorators
 
 @decorator
 def getx_funcy(call):
@@ -32,6 +34,13 @@ def getx_old_school(func):
         return x
     return wrapper
 
+@wrapt.decorator
+def getx_wrapt(wrapped, instance, args, kwargs):
+    if 'x' in kwargs:
+        return kwargs[x]
+    else:
+        return args[0]
+
 
 def undecorated():
     return sum(range(20))
@@ -42,8 +51,9 @@ funcy_decorated.__name__ = 'funcy_decorated'
 old_school_decorated = empty_old_school(undecorated)
 old_school_decorated.__name__ = 'old_school_decorated'
 
-# wrapt_decorated = empty_wrapt(undecorated)
-# wrapt_decorated.__name__ = 'wrapt_decorated'
+@empty_wrapt
+def wrapt_decorated():
+    return sum(range(20))
 
 
 def undecorated_getx(x):
@@ -53,7 +63,11 @@ funcy_getx = getx_funcy(undecorated_getx)
 funcy_getx.__name__ = 'funcy_getx'
 
 old_school_getx = getx_old_school(undecorated_getx)
-old_school_getx.__name__ = 'funcy_getx'
+old_school_getx.__name__ = 'old_school_getx'
+
+@getx_wrapt
+def wrapt_getx(x):
+    return x
 
 
 def bench_test(test, *args):
@@ -83,23 +97,90 @@ def bench_once(test, n, *args):
 
 def format_time(sec):
     return '%07d ns' % (sec * 1e9)
-    if sec < 1e-6:
-        return '%s ns' % (sec * 1e9)
-    elif sec < 1e-3:
-        return '%s µs' % (sec * 1e6)
-    elif sec < 1:
-        return '%s ms' % (sec * 1e3)
-    else:
-        return '%s s' % sec
 
 from profilehooks import profile
 
 
+# def make_decorator(deco, dargs=(), dkwargs={}):
+#     def _decorator(func):
+#         def wrapper(*args, **kwargs):
+#             call = Call(func, args, kwargs)
+#             return deco(call, *dargs, **dkwargs)
+#         return wraps(func)(wrapper)
+#     return _decorator
+
+WRAPPER_TEMPLATE = """
+def __wrapper_fab(__deco, __func):
+    def __wrapper{spec}:
+        __call = lambda: __func{spec}
+        __call._func = __func
+        __call._args = __func
+        __call._kwargs = __func
+{assigns}
+        return __deco(__call)
+    return __wrapper
+""".strip()
+
+def compile_template(func):
+    try:
+        spec = inspect.getargspec(func)
+    except TypeError:
+        spec = ArgSpec((), 'args', 'kwargs', None)
+
+    # Slicing with [1:-1] to get rid of parentheses
+    spec_str = inspect.formatargspec(*spec)
+
+    assigns = '\n'.join("        __call.{0} = {0}".format(arg) for arg in spec.args)
+    func_str = WRAPPER_TEMPLATE.format(name=func.__name__, spec=spec_str, assigns=assigns)
+
+    env = {}
+    # print func_str
+    exec func_str in env
+    return env['__wrapper_fab']
+
+def dec2(deco):
+    def _decorator(func):
+        template = compile_template(func)
+        return wraps(func)(template(deco, func))
+    return wraps(deco)(_decorator)
+
+@dec2
+def empty_dec2(call):
+    return call()
+
+dec2_decorated = empty_dec2(undecorated)
+dec2_decorated.__name__ = 'dec2_decorated'
+
+@dec2
+def getx_dec2(call):
+    return call.x
+
+dec2_getx = getx_dec2(undecorated_getx)
+dec2_getx.__name__ = 'dec2_getx'
+
+# def undecorated(x, y=100):
+#     return x * y
+
+# dec2 = decorator2(undecorated)
+# print dec2(42)
+# print dec2.__name__
+# print inspect.getargspec(dec2)
+
+# print dec2_getx(42)
+
 # bench_test(undecorated)
 # bench_test(old_school_decorated)
 # bench_test(funcy_decorated)
-# # bench_test(wrapt_decorated)
+# bench_test(wrapt_decorated)
+# bench_test(dec2_decorated)
 
 # bench_test(undecorated_getx, 42)
 # bench_test(old_school_getx, 42)
-bench_test(funcy_getx, 42)
+# bench_test(funcy_getx, 42)
+# bench_test(wrapt_getx, 42)
+# bench_test(dec2_getx, 42)
+
+bench_test(empty_old_school, undecorated)
+bench_test(empty_funcy, undecorated)
+bench_test(empty_wrapt, undecorated)
+bench_test(empty_dec2, undecorated)
