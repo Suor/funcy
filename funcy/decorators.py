594026a5b4794be201897b6e6882c387b7e7bf1b
Alexander Schepanovski <suor.web@gmail.com>
2017-05-10 16:29:11 +0700

    Remove unused variable

--- funcy/decorators.py
+++ funcy/decorators.py
@@ -73,7 +73,6 @@ def arggetter(func, _cache={}):
         return _cache[func]
 
     argnames = get_argnames(func)
-    argcount = len(argnames)
     indexes = dict((name, i) for i, name in enumerate(argnames))
     if func.__defaults__:
         defaults = dict((argnames[-1-i], v) for i, v in enumerate(func.__defaults__))
