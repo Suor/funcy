5a60f89f4ab2737a98e7870f43cb36ec58e81698
Alexander Schepanovski <suor.web@gmail.com>
2017-08-23 21:49:14 +0700

    Fill in funcs docstring

--- funcy/funcs.py
+++ funcy/funcs.py
@@ -15,28 +15,30 @@ __all__ = ['identity', 'constantly', 'caller',
 
 
 def identity(x):
+    """Returns its argument."""
     return x
 
 def constantly(x):
+    """Creates a function accepting any args, but always returning x."""
     return lambda *a, **kw: x
 
 # an operator.methodcaller() brother
 def caller(*a, **kw):
+    """Creates a function calling its sole argument with given *a, **kw."""
     return lambda f: f(*a, **kw)
 
-# not using functools.partial to get real function
 def func_partial(func, *args, **kwargs):
-    """
-    A functools.partial alternative, which returns a real function.
-    Can be used to construct methods.
-    """
+    """A functools.partial alternative, which returns a real function.
+       Can be used to construct methods."""
     return lambda *a, **kw: func(*(args + a), **dict(kwargs, **kw))
 
 def rpartial(func, *args):
+    """Partially applies last arguments."""
     return lambda *a: func(*(a + args))
 
 
 def curry(func, n=EMPTY):
+    """Curries func into a chain of one argument functions."""
     if n is EMPTY:
         _, n, _ = get_spec(func)
 
@@ -49,6 +51,8 @@ def curry(func, n=EMPTY):
 
 
 def rcurry(func, n=EMPTY):
+    """Curries func into a chain of one argument functions.
+       Arguments are passed from right to left."""
     if n is EMPTY:
         _, n, _ = get_spec(func)
 
@@ -62,6 +66,8 @@ def rcurry(func, n=EMPTY):
 
 # TODO: drop `n` in next major release
 def autocurry(func, n=EMPTY, _spec=None, _args=(), _kwargs={}):
+    """Creates a version of func returning its partial applications
+       until sufficient arguments are passed."""
     spec = _spec or (get_spec(func) if n is EMPTY else (set(), n, n))
     required_names, required_n, max_n = spec
 
@@ -84,6 +90,7 @@ def autocurry(func, n=EMPTY, _spec=None, _args=(), _kwargs={}):
 
 
 def iffy(pred, action=EMPTY, default=identity):
+    """Creates a function, which conditionally applies action or default."""
     if action is EMPTY:
         return iffy(bool, pred)
     else:
@@ -93,6 +100,7 @@ def iffy(pred, action=EMPTY, default=identity):
 
 
 def compose(*fs):
+    """Composes passed functions."""
     if fs:
         pair = lambda f, g: lambda *a, **kw: f(g(*a, **kw))
         return reduce(pair, imap(make_func, fs))
@@ -100,9 +108,11 @@ def compose(*fs):
         return identity
 
 def rcompose(*fs):
+    """Composes functions, calling them from left to right."""
     return compose(*reversed(fs))
 
 def complement(pred):
+    """Constructs a complementary predicate."""
     return compose(__not__, pred)
 
 
@@ -110,9 +120,13 @@ def complement(pred):
 #       from all calls to i?juxt result since map iterator will be depleted
 
 def juxt(*fs):
+    """Constructs a juxtaposition of the given functions.
+       Result returns a list of results of fs."""
     extended_fs = map(make_func, fs)
     return lambda *a, **kw: [f(*a, **kw) for f in extended_fs]
 
 def ijuxt(*fs):
+    """Constructs a lazy juxtaposition of the given functions.
+       Result returns an iterator of results of fs."""
     extended_fs = map(make_func, fs)
     return lambda *a, **kw: (f(*a, **kw) for f in extended_fs)
