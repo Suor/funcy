ea97ad5378495f9abcff0b2923a115b8ed31f2ad
Alexander Schepanovski <suor.web@gmail.com>
2018-07-09 12:51:30 +0700

    Make code look nice in docs

--- docs/conf.py
+++ docs/conf.py
@@ -104,7 +104,7 @@ if not on_rtd:  # only import and set the theme if we're building docs locally
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_style = 'overrides.css'
+# html_style = '...'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -270,4 +270,5 @@ class DecoratorDocumenter(FunctionDocumenter):
 
 
 def setup(app):
+    app.add_stylesheet('overrides.css')
     app.add_autodocumenter(DecoratorDocumenter)
