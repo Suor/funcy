61fc6a9494199c539390a0776c43d22e127cc5a8
Alexander Schepanovski <suor.web@gmail.com>
2021-12-20 12:32:41 +0700

    Fix setup.py README/long_description stuff
    - remove Gitter replaces
    - explicitly set long_description_content_type

--- setup.py
+++ setup.py
@@ -3,9 +3,7 @@ from setuptools import setup
 
 # Remove build status and move Gitter link under title for PyPi
 README = open('README.rst').read()    \
-    .replace('|Build Status|', '', 1) \
-    .replace('|Gitter|', '', 1)       \
-    .replace('===\n', '===\n\n|Gitter|\n')
+    .replace('|Build Status|', '', 1)
 
 
 setup(
@@ -16,6 +14,7 @@ setup(
 
     description='A fancy and practical functional tools',
     long_description=README,
+    long_description_content_type="text/x-rst",
     url='http://github.com/Suor/funcy',
     license='BSD',
 
