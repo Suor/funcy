cf6a0dbb65221a18fbe4474c673496aa58d2ee63
Alexander Schepanovski <suor.web@gmail.com>
2024-05-01 16:26:35 +0700

    Fix docs

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,12 @@ needs_sphinx = '3.5.3'
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.coverage', 'sphinx.ext.intersphinx', 'sphinx.ext.autodoc']
+extensions = [
+    'sphinx.ext.coverage',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.autodoc',
+    'sphinxcontrib.jquery'
+]
 
 intersphinx_mapping = {
     'py2': ('http://docs.python.org/2', None),
