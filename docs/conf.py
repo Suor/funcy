62d6c40d2d68e70da16ba6203f03b9df9ed3273b
Alexander Schepanovski <suor.web@gmail.com>
2024-05-01 17:05:33 +0700

    Clean up docs/conf

--- docs/conf.py
+++ docs/conf.py
@@ -103,10 +103,7 @@ pygments_style = 'sphinx'
 
 # -- Options for HTML output ---------------------------------------------------
 
-# if not on_rtd:  # only import and set the theme if we're building docs locally
-#     import sphinx_rtd_theme
 html_theme = "sphinx_rtd_theme"
-    # html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
